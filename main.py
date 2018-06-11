4be5f07d39dac6572a93e15d95c4a97582791446
marceamaps <marcea.irene@gmail.com>
2018-06-10 20:03:18 -0400

    got main.py working with caesar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-#from caesar import rotate_string
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True  
@@ -8,32 +8,36 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                form {{
                     background-color: #eee;
                     padding: 20px;
                     margin: 0 auto;
                     width: 540px;
                     font: 16px sans-serif;
                     border-radius: 10px
-                }
-                textarea {
+                }}
+                textarea {{
                     margin: 10px 0;
                     width: 540px;
                     height: 120px;
-                }
+                }}
+                .button {{
+                    margin: 10px 0;
+                    color: purple;
+                }}
             </style>
         </head>
         <body>
-        <form action="/web-caesar" method="post">
+        <form method="post">
             <label for="rot" name="rot" value="rot"> Rotate By
 
             <input type="text" name="rot"/ value="0" id="rot">
 
-            <input type="textarea" name="text"/>
+            <textarea name="textarea"> {0} </textarea>
 
             </label>
 
-        <input type="submit" value="Submit Query/> 
+        <input type="submit" value="Submit Query" class="button" /> 
 
         </form>
         </body>
@@ -42,17 +46,24 @@ form = """
 
 @app.route("/")
 def index():
-    
-    # rot = request.form['rot']
 
-    # text_area = request.form['text']
-
-    return form
+    return form.format(' ')
 
 
+@app.route("/", methods=['POST'])
+def encrypt():
 
+    rot = request.form['rot']
+    rot_num = int(rot)
 
+    text_area = request.form['textarea']
+    text_string = str(text_area)
 
+    encrpytion = rotate_string(text_string, rot_num)
+
+    content = "<h1>" + form.format(encrpytion) + "</h1>"
+
+    return content
 
 
 app.run()
